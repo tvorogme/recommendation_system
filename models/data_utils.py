a59d8d782f083e17b598285aecd94efafebcb610
Andrey Tvorozhkov <atvorozhkov@yandex.ru>
2016-06-26 17:25:07 +0300

    Add data utils to work with data (#2)
    * tvran to tvrain
    
    * Data to dataframe
    
    * Fix typos
    
    * Add data utils to work with data
    
    * Fix template

---
+++ models/data_utils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+import pandas as pd
+import os
+import random
+from pymongo import MongoClient
+
+
+class TvrainData:
+    def __init__(self):
+        '''
+        :param data: Pandas Frame with data.
+        Just load data from Mongo.
+        '''
+        mongodb_items = list(MongoClient(os.environ['MONGODB_URL']).tvrain.tvrain.find())
+        self.dataframe = pd.DataFrame.from_dict(mongodb_items)
+        
+        # This shit is about mongo can't save id -> str.
+        self.dataframe._id = list(map(lambda _id: str(_id), self.dataframe._id.values))
+
+    def get_random_articles(self, n):
+        '''Returns N of topics for index.html'''
+        sorted_time = sorted(self.dataframe.time.values)
+        # We need indexes for slice in time
+        index = sorted_time.index(random.choice(sorted_time))
+        times = sorted_time[index:index+n]
+
+        articles = []
+        for val in self.dataframe[self.dataframe.time.isin(times)].values:
+            articles.append({'_id': val[0], 'title': val[3]})
+
+        return articles
