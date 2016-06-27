d95d1866273bfcfe18ee611ad2fd08ccada9e60b
xenx <atvorozhkov@yandex.ru>
2016-06-27 18:22:29 +0300

    Fix

--- data/data_utils.py
+++ data/data_utils.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 import os
-import random
 from pymongo import MongoClient
-
+import random
 
 class TvrainData():
     def __init__(self):
@@ -11,24 +10,13 @@ class TvrainData():
         :param data: Pandas Frame with data.
         Just load data from Mongo.
         """
-        mongodb_items = list(MongoClient(os.environ['MONGODB_URL']).tvrain.tvrain.find())
-        self.dataframe = pd.DataFrame.from_dict(mongodb_items)
-
-        # This shit is about mongo can't save id -> str.
-        self.dataframe._id = list(map(str, self.dataframe._id.values))
+        self.collection = MongoClient(os.environ['MONGODB_URL']).tvrain.articles
+        self.collection.create_index("time")
 
     def get_random_articles(self, n):
         """Returns N of topics for index.html"""
-        sorted_time = sorted(self.dataframe.time.values)
-        # We need indexes for slice in time
-        index = sorted_time.index(random.choice(sorted_time))
-        times = sorted_time[index:index+n]
-
-        articles = []
-        for val in self.dataframe[self.dataframe.time.isin(times)].values:
-            articles.append({'_id': val[0], 'title': val[3]})
-
-        return articles
+        articles = self.collection.find({}, {'url': 1, 'time': 1, 'title': 1}).skip(random.randint(0,self.collection.count())).limit(10)
+        return list(articles)
 
     def get_articles_data(self, articles_ids):
         """
