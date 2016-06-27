271cfbf2d736439d28c04dbdc599ac48f7e25a7a
Seva Zhidkov <zhidkovseva@gmail.com>
2016-06-27 15:22:07 +0300

    Add script for loading topics to mongodb (#7)
    * Add script for loading topics to mongodb
    
    * Add os to imports

---
+++ scripts/load_topics_to_mongodb.py
@@ -0,0 +1,22 @@
+import os
+import csv
+from pymongo import MongoClient
+
+print('Parsing topics')
+topics = {}
+with open('topics.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        if line[0] == 1:
+            continue
+        topics[line[0]] = line[1:]
+
+print('Connecting to MongoDB')
+mongodb_client = MongoClient(os.environ['MONGODB_URL'])
+db = mongodb_client.tvrain
+articles = db.articles
+
+for article in topics:
+    articles.update({'_id': article}, {'$set': {
+        'topics': topics[article]
+    }})
