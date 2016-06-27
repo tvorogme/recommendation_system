58c63bd7650541c8a909a55b2306970b20119572
Seva Zhidkov <zhidkovseva@gmail.com>
2016-06-27 15:45:48 +0300

    Basic model (#8)
    * Add script for loading topics to mongodb
    
    * Add os to imports
    
    * Add basic random model

--- data/data_utils.py
+++ data/data_utils.py
@@ -8,10 +8,10 @@ from pymongo import MongoClient
 
 class TvrainData():
     def __init__(self):
-        '''
+        """
         :param data: Pandas Frame with data.
         Just load data from Mongo.
-        '''
+        """
         mongodb_items = list(MongoClient(os.environ['MONGODB_URL']).tvrain.tvrain.find())
         self.dataframe = pd.DataFrame.from_dict(mongodb_items)
 
@@ -19,7 +19,7 @@ class TvrainData():
         self.dataframe._id = list(map(str, self.dataframe._id.values))
 
     def get_random_articles(self, n):
-        '''Returns N of topics for index.html'''
+        """Returns N of topics for index.html"""
         sorted_time = sorted(self.dataframe.time.values)
         # We need indexes for slice in time
         index = sorted_time.index(random.choice(sorted_time))
@@ -30,4 +30,20 @@ class TvrainData():
             articles.append({'_id': val[0], 'title': val[3]})
 
         return articles
-    
+
+    def get_articles_data(self, articles_ids):
+        """
+        Get data from MongoDB for articles_ids
+        :param articles_ids: ['article_id', ...]
+        :return: list of MongoDB documents
+        """
+        raise NotImplemented()
+
+    def iterate_articles(self, except_articles):
+        """
+        Iteate throw all articles without ids of except articles
+        :param except_articles: list of ids
+        :return:
+        """
+        raise NotImplemented()
+
