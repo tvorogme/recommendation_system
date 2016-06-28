d6cc244e0c8b98b59a42f8d09f35d291f7d583cf
Seva Zhidkov <zhidkovseva@gmail.com>
2016-06-28 13:14:32 +0300

    Add script for loading sequences into MongoDB

---
+++ scripts/load_sequences_to_mongodb.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import json
+from pymongo import MongoClient
+
+print('Parsing sequences')
+with open(sys.argv[1], 'r') as sequences_file:
+    sequences = json.loads(sequences_file)
+
+print('Connecting to MongoDB')
+mongodb_client = MongoClient(os.environ['MONGODB_URL'])
+db = mongodb_client.tvrain
+sequences_collection = db.sequences
+
+for sequence in sequences:
+    sequences_collection.insert_one({
+        'urls': sequence
+    })
