f269a9f8030c0a01332106200006daa91571dfa5
xenx <atvorozhkov@yandex.ru>
2016-06-28 14:19:18 +0300

    Add nmf model

--- data/data_utils.py
+++ data/data_utils.py
@@ -10,13 +10,19 @@ class TvrainData():
         :param data: Pandas Frame with data.
         Just load data from Mongo.
         """
+        self.sequences = MongoClient(os.environ['MONGODB_URL']).tvrain.sequences
         self.collection = MongoClient(os.environ['MONGODB_URL']).tvrain.articles
         self.collection.create_index("time")
 
+
     def get_random_articles(self, n):
         """Returns N of topics for index.html"""
-        articles = self.collection.find({}, {'url': 1, 'time': 1, 'title': 1}).skip(random.randint(0,self.collection.count())).limit(10)
+        articles = self.collection.find().skip(random.randint(0,self.collection.count())).limit(n)
         return list(articles)
+
+    def get_article_id(self, url):
+        """Get id by url"""
+        self.collection.find({'url': url}).limit(-1)
 
     def get_articles_data(self, articles_ids):
         """
@@ -30,14 +36,17 @@ class TvrainData():
 
         return articles_data
 
-    def iterate_articles(self, except_articles):
+    def iterate_articles(self, except_articles=[], skip=0):
         """
         Iteate throw all articles without ids of except articles
         :param except_articles: list of ids
         :return:
         """
-        for value in self.collection.find().limit(-1):
-            yield value
+        for value in self.collection.find().skip(skip).limit(-1):
+            if value not in except_articles:
+                yield value
 
-
+    def get_sequences(self):
+        """Return all sequences for train"""
+        return list(self.sequences.find().limit(-1))
 
